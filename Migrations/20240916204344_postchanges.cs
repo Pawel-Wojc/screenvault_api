d3cdb6b67ce0c701dd123d04ed07e08a52b227f9
pawel-pawel <pawelitwojcik@gmail.com>
2024-09-17 09:57:25 +0200

    db changes

---
+++ Migrations/20240916204344_postchanges.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace api_screenvault.Migrations
+{
+    /// <inheritdoc />
+    public partial class postchanges : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Link",
+                table: "Posts",
+                newName: "Uri");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsAnonymous",
+                table: "Posts",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LinkId",
+                table: "Posts",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsAnonymous",
+                table: "Posts");
+
+            migrationBuilder.DropColumn(
+                name: "LinkId",
+                table: "Posts");
+
+            migrationBuilder.RenameColumn(
+                name: "Uri",
+                table: "Posts",
+                newName: "Link");
+        }
+    }
+}
