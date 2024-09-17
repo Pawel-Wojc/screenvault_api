f418d34f8c7e0666998096ade22494f0f14ac030
pawel-pawel <pawelitwojcik@gmail.com>
2024-09-17 10:00:20 +0200

    Create AnonymousPostService.cs

---
+++ Services/AnonymousPostService.cs
@@ -0,0 +1,40 @@
+﻿using api_screenvault.Dto;
+using api_screenvault.Helpers;
+using api_screenvault.Model;
+using Microsoft.AspNetCore.Http.HttpResults;
+
+namespace api_screenvault.Services
+{
+    public interface IAnonymousPostService {
+        Task<string> CreateAnonymousPost(IFormFile file, string title);
+
+
+    }
+    public class AnonymousPostService : IAnonymousPostService
+    {
+        private readonly IAnonymousAzureBlobHandling _anonymousAzureBlobHandling;
+        private readonly ISharedPostIdGenerator _sharedPostIdGenerator;
+        public AnonymousPostService(IAnonymousAzureBlobHandling anonymousAzureBlobHandling, ISharedPostIdGenerator sharedPostIdGenerator)
+        {
+            _anonymousAzureBlobHandling = anonymousAzureBlobHandling;
+            _sharedPostIdGenerator = sharedPostIdGenerator;
+        }
+
+        public async Task<string> CreateAnonymousPost(IFormFile file, string title) {
+
+            BlobResponseDto  blobResponse = await _anonymousAzureBlobHandling.UploadAnonymousAsyn(file);
+            if (blobResponse.Error) {
+                return "cant create"+ blobResponse.Status; 
+            }
+
+            Post post = new() { Id = new Guid(), Title = title, IsAnonymous = true };
+
+            post.LinkId = _sharedPostIdGenerator.GenerateUniqueLinkId();
+            post.Uri = blobResponse.Blob.Uri;
+            return post.LinkId;
+
+        }
+
+       
+    }
+}
