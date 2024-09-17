991dc18e176106d842bcd20d26e032c4059eb962
pawel-pawel <pawelitwojcik@gmail.com>
2024-09-17 09:58:28 +0200

    anonymous post model update

--- Model/Post.cs
+++ Model/Post.cs
@@ -7,11 +7,14 @@
         public string? Title { get; set; }
         public int? Views { get; set; }
         public int? Votes { get; set; }
+        public bool IsAnonymous { get; set; }
         public ICollection<string>? Tags { get; set; }
-        public string? Link { get; set; } //a 6 digits and letters code to store image
+        public string? Uri { get; set; }
+        public string? LinkId { get; set; } //a 6 digits and letters code to store image
         public DateTime CreatedAt { get; set; }
 
         public Post() {
+
             Views = 0;
             Votes = 0;
             CreatedAt = DateTime.Now;
