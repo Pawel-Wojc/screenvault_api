e7cb6620405d750cae4c4e6afc9d83b2b3e31fc3
pawel-pawel <pawelitwojcik@gmail.com>
2024-09-20 23:12:58 +0200

    data base update

--- Migrations/20240919105041_postcolumnamingchange.Designer.cs
+++ Migrations/20240919105041_postcolumnamingchange.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using api_screenvault.Data;
 
@@ -11,9 +12,11 @@ using api_screenvault.Data;
 namespace api_screenvault.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240919105041_postcolumnamingchange")]
+    partial class postcolumnamingchange
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -177,7 +180,7 @@ namespace api_screenvault.Migrations
 
                     b.HasIndex("UserId");
 
-                    b.ToTable("Comments", (string)null);
+                    b.ToTable("Comments");
                 });
 
             modelBuilder.Entity("api_screenvault.Model.Post", b =>
@@ -217,7 +220,7 @@ namespace api_screenvault.Migrations
 
                     b.HasIndex("UserId");
 
-                    b.ToTable("Posts", (string)null);
+                    b.ToTable("Posts");
                 });
 
             modelBuilder.Entity("api_screenvault.Model.User", b =>
