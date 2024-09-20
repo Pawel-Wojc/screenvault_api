7870e2b3995459efdf61394a33d4a901682786ca
pawel-pawel <pawelitwojcik@gmail.com>
2024-09-20 23:12:43 +0200

    adding helpers

---
+++ Helpers/ExtensionChecker.cs
@@ -0,0 +1,15 @@
+﻿namespace api_screenvault.Helpers
+{
+    public static class ExtensionChecker
+    {
+        public  static readonly List<string> allowedExtensions = new List<string> { ".jpg", ".jpeg", ".png", ".gif", ".bmp", ".tiff", ".tif", ".webp", ".svg", ".heic", ".ico" };
+        public static bool ExtensionIsValid(string fileName) { 
+            var extension = System.IO.Path.GetExtension(fileName);
+            if (allowedExtensions.All(ext => ext != extension))
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
